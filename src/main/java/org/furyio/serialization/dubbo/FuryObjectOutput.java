ecb8d0b78c954540cb17f920a08b710be1f8a1d6
mubai <chaokun.yck@antgroup.com>
2023-08-20 11:17:57 +0800

    use eager write/read to avoid nested FuryObjectOutput creation

--- src/main/java/org/furyio/serialization/dubbo/FuryObjectOutput.java
+++ src/main/java/org/furyio/serialization/dubbo/FuryObjectOutput.java
@@ -1,6 +1,5 @@
 package org.furyio.serialization.dubbo;
 
-import com.google.common.base.Preconditions;
 import io.fury.Fury;
 import io.fury.memory.MemoryBuffer;
 import java.io.IOException;
@@ -21,79 +20,72 @@ public class FuryObjectOutput implements ObjectOutput {
     this.fury = fury;
     this.buffer = buffer;
     this.output = output;
-    if (buffer.writerIndex() != 0) {
-      throw new IllegalArgumentException("Index should be 0 instead of " + buffer.writerIndex());
-    }
-    buffer.writeInt(-1);
   }
 
   public void writeObject(Object obj) {
-    fury.serializeJavaObjectAndClass(buffer, obj);
+    fury.serializeJavaObjectAndClass(output, obj);
   }
 
-  public void writeBool(boolean v) {
-    buffer.writeBoolean(v);
+  public void writeBool(boolean v) throws IOException {
+    buffer.unsafePutBoolean(0, v);
+    output.write(buffer.getHeapMemory(), 0, 1);
   }
 
-  public void writeByte(byte v) {
-    buffer.writeByte(v);
+  public void writeByte(byte v) throws IOException {
+    buffer.unsafePut(0, v);
+    output.write(buffer.getHeapMemory(), 0, 1);
   }
 
-  public void writeShort(short v) {
-    buffer.writeShort(v);
+  public void writeShort(short v) throws IOException {
+    buffer.unsafePutShort(0, v);
+    output.write(buffer.getHeapMemory(), 0, 2);
   }
 
-  public void writeInt(int v) {
-    buffer.writeVarInt(v);
+  public void writeInt(int v) throws IOException {
+    buffer.unsafePutInt(0, v);
+    output.write(buffer.getHeapMemory(), 0, 4);
   }
 
-  public void writeLong(long v) {
-    buffer.writeLong(v);
+  public void writeLong(long v) throws IOException {
+    buffer.unsafePutLong(0, v);
+    output.write(buffer.getHeapMemory(), 0, 8);
   }
 
-  public void writeFloat(float v) {
-    buffer.writeFloat(v);
+  public void writeFloat(float v) throws IOException {
+    buffer.unsafePutFloat(0, v);
+    output.write(buffer.getHeapMemory(), 0, 4);
   }
 
-  public void writeDouble(double v) {
-    buffer.writeDouble(v);
+  public void writeDouble(double v) throws IOException {
+    buffer.unsafePutDouble(0, v);
+    output.write(buffer.getHeapMemory(), 0, 8);
   }
 
-  public void writeUTF(String v) {
+  public void writeUTF(String v) throws IOException {
+    // avoid `writeInt` overwrite sting data.
+    buffer.writerIndex(4);
     if (v != null) {
       buffer.writeBoolean(true);
       fury.writeJavaString(buffer, v);
     } else {
       buffer.writeBoolean(false);
     }
+    int size = buffer.writerIndex() - 4;
+    writeInt(size);
+    output.write(buffer.getHeapMemory(), 4, size);
   }
 
-  public void writeBytes(byte[] v) {
-    if (v != null) {
-      buffer.writeBoolean(true);
-      buffer.writeBytesWithSizeEmbedded(v);
-    } else {
-      buffer.writeBoolean(false);
-    }
+  public void writeBytes(byte[] v) throws IOException {
+    writeInt(v.length);
+    output.write(v);
   }
 
-  public void writeBytes(byte[] v, int off, int len) {
-    if (v != null) {
-      buffer.writeBoolean(true);
-      buffer.writePositiveVarInt(len);
-      buffer.writeBytes(v, off, len);
-    } else {
-      buffer.writeBoolean(false);
-    }
+  public void writeBytes(byte[] v, int off, int len) throws IOException {
+    writeInt(len);
+    output.write(v, off, len);
   }
 
   public void flushBuffer() throws IOException {
-    byte[] heapMemory = buffer.getHeapMemory();
-    Preconditions.checkNotNull(heapMemory);
-    final int targetIndex = buffer.unsafeHeapReaderIndex();
-    buffer.putInt(0, buffer.writerIndex() - 4);
-    output.write(heapMemory, targetIndex, buffer.writerIndex());
-    buffer.writerIndex(0);
     output.flush();
   }
 }
