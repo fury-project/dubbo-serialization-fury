391719753981357b322077051b0f27a3b21d5f8a
mubai <chaokun.yck@antgroup.com>
2023-09-01 14:51:21 +0800

    integrate dubbo black/white list with fury checker

--- src/main/java/org/furyio/serialization/dubbo/BaseFurySerialization.java
+++ src/main/java/org/furyio/serialization/dubbo/BaseFurySerialization.java
@@ -6,10 +6,12 @@ import io.fury.memory.MemoryBuffer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Optional;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.serialize.ObjectInput;
 import org.apache.dubbo.common.serialize.ObjectOutput;
 import org.apache.dubbo.common.serialize.Serialization;
+import org.apache.dubbo.rpc.model.FrameworkModel;
 
 /**
  * Fury serialization framework integration with dubbo.
@@ -21,11 +23,26 @@ public abstract class BaseFurySerialization implements Serialization {
 
   public ObjectOutput serialize(URL url, OutputStream output) throws IOException {
     Tuple2<Fury, MemoryBuffer> tuple2 = getFury();
-    return new FuryObjectOutput(tuple2.f0, tuple2.f1, output);
+    Fury fury = tuple2.f0;
+    FuryCheckerListener checkerListener = getCheckerListener(url);
+    fury.getClassResolver().setClassChecker(checkerListener.getChecker());
+    fury.getClassResolver().setSerializerFactory(checkerListener);
+    return new FuryObjectOutput(fury, tuple2.f1, output);
   }
 
   public ObjectInput deserialize(URL url, InputStream input) throws IOException {
     Tuple2<Fury, MemoryBuffer> tuple2 = getFury();
-    return new FuryObjectInput(tuple2.f0, tuple2.f1, input);
+    Fury fury = tuple2.f0;
+    FuryCheckerListener checkerListener = getCheckerListener(url);
+    fury.getClassResolver().setClassChecker(checkerListener.getChecker());
+    return new FuryObjectInput(fury, tuple2.f1, input);
+  }
+
+  private static FuryCheckerListener getCheckerListener(URL url) {
+    return Optional.ofNullable(url)
+        .map(URL::getOrDefaultFrameworkModel)
+        .orElseGet(FrameworkModel::defaultModel)
+        .getBeanFactory()
+        .getBean(FuryCheckerListener.class);
   }
 }
